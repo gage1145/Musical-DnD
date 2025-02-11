665e4872451592bed3759f85b4abc998fbdb89b9
Gage Rowden <140093724+gage1145@users.noreply.github.com>
2025-02-11 13:55:43 -0600

    Added weights to the modes.

--- app.R
+++ app.R
@@ -14,8 +14,9 @@ progressions <- c("I-V-vi-IV", "I-IV-V-I", "ii-V-I", "I-vi-IV-V", "Uncommon")
 moods <- c("Surprised", "Stressed", "Mad", "Loved", "Lonely", "Weird", "Uncomfortable", "Touched", "Tired", "Thankful", "Sympathetic", "Smart", "Sleepy", "Silly", "Pleased", "Pessimistic", "Sick", "Shocked", "Satisfied", "Sad", "Rushed", "Restless", "Relieved", "Relaxed", "Rejuvenated", "Rejected", "Refreshed", "Recumbent", "Quixotic", "Predatory", "Peaceful", "Optimistic", "Okay", "Numb", "Giggly", "Giddy", "Not specified", "Nerdy", "Infuriated", "Indifferent", "Indescribable", "Naughty", "Morose", "Moody", "Mischievous", "Mellow", "Melancholy", "Listless", "Lethargic", "Lazy", "Jubilant", "Jealous", "Irritated", "Irate", "Impressed", "Hyper", "Hungry", "Hot", "Hopeful", "High", "Happy", "Guilty", "Grumpy", "Groggy", "Grateful", "Dorky", "Ditzy", "Discontent", "Good", "Gloomy", "Geeky", "Full", "Frustrated", "Flirty", "Exhausted", "Excited", "Enraged", "Energetic", "Anxious", "Annoyed", "Angry", "Ecstatic", "Drunk", "Drained", "Disappointed", "Dirty", "Devious", "Determined", "Depressed", "Dark", "Cynical", "Curious", "Alone", "Aggravated", "Accomplished", "Accepted", "Crushed", "Crazy", "Crappy", "Cranky", "Content", "Confused", "Complacent", "Cold", "Chipper", "Cheerful", "Calm", "Bouncy", "Bored", "Blissful", "Blank", "Blah", "Bittersweet", "Bewildered", "Awake", "Ashamed", "Apathetic", "Amused", "Exanimate", "Envious", "Enthralled")
 
 # Weights
-time_weights <- c(0.7, 0.15, 0.1, 0.01, 0.01, 0.01, 0.01, 0.01)
+mode_weights <- c(0.3, 0.25, 0.2, 0.15, 0.05, 0.03, 0.02)
 tempo_weights <- c(0.25, 0.4, 0.25, 0.1)
+time_weights <- c(0.7, 0.15, 0.1, 0.01, 0.01, 0.01, 0.01, 0.01)
 progression_weights <- c(0.3, 0.2, 0.15, 0.15, 0.2)
 
 # UI
@@ -26,7 +27,7 @@ ui <- fluidPage(
       actionButton("roll", "Roll for Parameters")
     ),
     mainPanel(
-      h3("Your Song Parameters:"),
+      h1("Your Song Parameters:"),
       verbatimTextOutput("output")
     )
   )
@@ -34,18 +35,21 @@ ui <- fluidPage(
 
 # Server
 server <- function(input, output) {
-  observeEvent(input$roll, {
-    song_params <- list(
-      Key = paste(sample(keys, 1), sample(modes, 1)),
-      Tempo = weighted_sample(tempo_ranges, tempo_weights),
-      Time_Signature = weighted_sample(time_signatures, time_weights),
-      Chord_Progression = weighted_sample(progressions, progression_weights),
-      Mood = sample(moods, 1)
-    )
-    output$output <- renderText({
-      paste(names(song_params), ":", unlist(song_params), collapse = "\n")
-    })
-  })
+  observeEvent(
+    input$roll,
+    {
+      song_params <- list(
+        Key = paste(sample(keys, 1), weighted_sample(modes, mode_weights)),
+        Tempo = weighted_sample(tempo_ranges, tempo_weights),
+        `Time Signature` = weighted_sample(time_signatures, time_weights),
+        `Chord Progression` = weighted_sample(progressions, progression_weights),
+        Mood = sample(moods, 1)
+      )
+      output$output <- renderText({
+        paste0(names(song_params), ": ", unlist(song_params), collapse = "\n")
+      })
+    }
+  )
 }
 
 # Run the app
